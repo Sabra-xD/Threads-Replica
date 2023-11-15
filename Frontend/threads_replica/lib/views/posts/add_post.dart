a9f256e8b61e9d7cbeaf4f7bf6dae01a8d1966bf
Goddemunchies <52329758+Goddemunchies@users.noreply.github.com>
2023-11-15 07:24:06 +0200

    Post Screen finalized

--- Frontend/threads_replica/lib/views/posts/add_post.dart
+++ Frontend/threads_replica/lib/views/posts/add_post.dart
@@ -82,19 +82,22 @@ class _PostScreenState extends State<PostScreen> {
             ),
           ]),
         ),
-        body: Column(
-          children: [
-            const Divider(thickness: 0.5),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-              child: GetBuilder<UserInfo>(
-                init: _userInfo,
-                builder: (controller) {
-                  if (controller.isLoading.value) {
-                    controller.fetchData();
-                    return CircularProgressIndicator();
-                  } else {
-                    return Row(
+        body: GetBuilder<UserInfo>(
+          init: _userInfo,
+          builder: (controller) {
+            if (controller.isLoading.value) {
+              return const Center(
+                  child: CircularProgressIndicator(
+                color: Colors.lightBlue,
+              ));
+            } else {
+              return Column(
+                children: [
+                  const Divider(thickness: 0.5),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 12, vertical: 12),
+                    child: Row(
                       children: [
                         CircleAvatar(
                           foregroundImage: NetworkImage(
@@ -111,109 +114,62 @@ class _PostScreenState extends State<PostScreen> {
                               fontWeight: FontWeight.bold, color: primaryColor),
                         ),
                       ],
-                    );
-                    ;
-                  }
-                },
-              ),
-              // // //The issue with this is that it reloads whenever we access something in the UI.
-              // FutureBuilder(
-              //     future: Future.wait(
-              //         [_userInfo.getUserName(), _userInfo.getUserProfile()]),
-              //     builder: (context, snapshot) {
-              //       if (snapshot.connectionState == ConnectionState.waiting) {
-              //         return const Center(
-              //           child: CircularProgressIndicator(),
-              //         );
-              //       } else {
-              //         if (snapshot.hasError) {
-              //           return Center(
-              //             child: Text(
-              //               "Error: ${snapshot.error}",
-              //               style: const TextStyle(
-              //                   color: primaryColor,
-              //                   fontWeight: FontWeight.w800,
-              //                   fontSize: 25),
-              //             ),
-              //           );
-              //         } else {
-              //           final img = snapshot.data![1] ??
-              //               "https://c8.alamy.com/comp/2E915TB/glitch-error-404-computer-page-anaglyph-glitch-404-banner-error-layout-effect-screen-2E915TB.jpg";
-
-              //           final username = snapshot.data?[0] ?? "Username";
-              //           return Row(
-              //             children: [
-              //               CircleAvatar(
-              //                 foregroundImage:
-              //                     NetworkImage(img), // Add a default image URL
-              //                 radius: 25,
-              //               ),
-              //               const SizedBox(
-              //                 width: 14,
-              //               ),
-              //               Text(
-              //                 username, // Replace with a default username or a placeholder
-              //                 style: const TextStyle(
-              //                     fontWeight: FontWeight.bold,
-              //                     color: primaryColor),
-              //               ),
-              //             ],
-              //           );
-              //         }
-              //       }
-              //     }),
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.19,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      child: Form(
-                        key: _formKey,
-                        child: TextFormField(
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return "Please enter the required text";
-                            }
-                          },
-                          controller: createPost.threadText,
-                          decoration: const InputDecoration(
-                            hintText: 'Start a thread...',
-                            hintStyle:
-                                TextStyle(fontSize: 14, color: Colors.grey),
-                            border: InputBorder.none,
-                          ),
-                          maxLines: null,
-                          style: const TextStyle(
-                              fontSize: 14, color: primaryColor),
-                          cursorColor: primaryColor,
-                        ),
-                      ),
                     ),
-                  ],
-                )
-              ],
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.18,
-                ),
-                IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.camera_alt,
-                      color: Colors.grey,
-                      size: 24,
-                    ))
-              ],
-            )
-          ],
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.19,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            child: Form(
+                              key: _formKey,
+                              child: TextFormField(
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return "Please enter the required text";
+                                  }
+                                },
+                                controller: createPost.threadText,
+                                decoration: const InputDecoration(
+                                  hintText: 'Start a thread...',
+                                  hintStyle: TextStyle(
+                                      fontSize: 14, color: Colors.grey),
+                                  border: InputBorder.none,
+                                ),
+                                maxLines: null,
+                                style: const TextStyle(
+                                    fontSize: 14, color: primaryColor),
+                                cursorColor: primaryColor,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.18,
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.camera_alt,
+                            color: Colors.grey,
+                            size: 24,
+                          ))
+                    ],
+                  )
+                ],
+              );
+            }
+          },
         ),
       ),
     );
